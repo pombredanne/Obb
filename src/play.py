6d372dfab8ef863bd4a31bee43b9e3196127aca0
Christopher Night <cosmologicon@gmail.com>
2011-09-11 03:33:47 -0400

    Day 1 screenshot

--- src/play.py
+++ src/play.py
@@ -10,7 +10,7 @@ class Play(context.Context):
         for part in self.body.parts:
             if isinstance(part, body.Organ):
                 px, py = part.screenpos()
-                ps.append(((px/10, py/10), 12))
+                ps.append(((px/10, py/10), 20))
         self.mask = vista.Mask((settings.sx/10, settings.sy/10), ps)
         self.mask = pygame.transform.smoothscale(self.mask.surf, settings.size)
 
@@ -18,15 +18,15 @@ class Play(context.Context):
         self.ton = vista.grid.tnearest(mousepos)
 
     def draw(self):
-#        vista.clear((0,0,0))
+        vista.clear((0,0,0))
 #        for _ in range(2000):
 #            x = random.randint(0,settings.sx)
 #            y = random.randint(0,settings.sy)
 #            vista.screen.set_at((x,y), (255, 255, 255))
-#        for x in range(-6, 6):
-#            for y in range(-6, 6):
-#                vista.grid.drawhex((x,y))
-        vista.clear()
+        for x in range(-6, 6):
+            for y in range(-6, 6):
+                vista.grid.drawhex((x,y))
+#        vista.clear()
         vista.grid.drawhex(self.ton, (255, 255, 255))
         self.body.draw()
         vista.screen.blit(self.mask, (0,0))
