88c07148a8eae23a9ce1cdb2f3e09a43c17fa95d
Christopher Night <cosmologicon@gmail.com>
2011-09-10 21:24:21 -0400

    Screen-to-tile conversions in HexGrid

--- src/play.py
+++ src/play.py
@@ -4,9 +4,13 @@ class Play(context.Context):
     def __init__(self):
         self.grid = vista.HexGrid()
 
+    def think(self, dt, events, keys, mousepos, buttons):
+        self.ton = self.grid.tnearest(mousepos)
+
     def draw(self):
         for x in range(-6, 6):
             for y in range(-6, 6):
                 self.grid.drawhex((x,y))
+        self.grid.drawhex(self.ton, (255, 255, 255))
 
 
--- src/vista.py
+++ src/vista.py
@@ -37,9 +37,31 @@ class HexGrid(object):
         py = self.y0 - s3 * (y + 0.5 * x + dy) * self.a
         return int(px + .5), int(py + .5)
 
+    def gedge(self, (x, y), e):
+        """Screen coordinate of center of eth edge of tile at (x,y)"""
+        dx, dy = [(0,-0.5), (0.75,-0.25), (0.75,0.25),
+                  (0,0.5), (-0.75,0.25), (-0.75,-0.25)][e%6]
+        px = self.x0 + (1.5 * x + dx) * self.a
+        py = self.y0 - s3 * (y + 0.5 * x + dy) * self.a
+        return int(px + .5), int(py + .5)
+
+    def opposite(self, (x, y), e):
+        """The tile and edge that's opposite the specified edge"""
+        dx, dy = [(0,1), (1,0), (1,-1), (0,-1), (-1,0), (-1,1)][e%6]
+        return (x+dx, y+dy), (e+3)%6
+
+    def tnearest(self, (px, py)):
+        """The tile that the given screen position is over"""
+        x0 = math.floor(float(px - self.x0) / self.a / 1.5)
+        y0 = math.floor(-float(py - self.y0) / self.a / s3 - 0.5 * x0)
+        d2 = lambda (x0, y0), (x1, y1): (x0 - x1) ** 2 + (y0 - y1) ** 2
+        d2s = [(d2((px, py), self.gcenter((x, y))), (x, y))
+                for x in (x0,x0+1) for y in (y0,y0+1)]
+        return min(d2s)[1]
+
     def drawhex(self, (x, y), color=None):
         if color is None:
-            color = [(255,0,0,64),(0,255,0,64),(0,0,255,64)][(y-x)%3]
+            color = [(64,0,0),(0,64,0),(0,0,64)][(y-x)%3]
         vs = [self.gvertex((x, y), v) for v in range(6)]        
         pygame.draw.polygon(screen, color, vs)
 
