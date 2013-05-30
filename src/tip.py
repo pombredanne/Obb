2e1b7913951950600dd541008f88b7d50d2d3108
mdtrooper <md@speccy-laptop.(none)>
2013-05-31 01:22:45 +0200

    Improved the source code style.

--- src/tip.py
+++ src/tip.py
@@ -4,34 +4,35 @@ import font, graphics, settings
 
 currenttip = ""
 def settip(newtip = ""):
-    global currenttip
-    currenttip = newtip
-    
+	global currenttip
+	currenttip = newtip
+
+
 ticker = 0
 def think(dt):
-    global surf, rect, ticker, bheight
-    import vista
-    if not settings.showtips: return
-    if not currenttip:
-        surf = None
-        ticker = max(ticker - dt, 0)
-    else:
-        surf = font.blocktext(currenttip, size = settings.layout.tipsize)
-        bheight = surf.get_height()
-        ticker = min(ticker + dt, 0.25)
-        
-    
+	global surf, rect, ticker, bheight
+	import vista
+	if not settings.showtips: return
+	if not currenttip:
+		surf = None
+		ticker = max(ticker - dt, 0)
+	else:
+		surf = font.blocktext(currenttip, size = settings.layout.tipsize)
+		bheight = surf.get_height()
+		ticker = min(ticker + dt, 0.25)
+
+
 def draw():
-    """Should be called after the main blit, even after the overlays"""
-    import vista
-    currenttip = ""
-    if not settings.showtips:
-        return
-    if ticker:
-        bubble = graphics.thoughtbubble(bheight, f = ticker*4)
-        bubblerect = bubble.get_rect(midtop = vista.vrect.midtop)
-        vista._screen.blit(bubble, bubblerect)
-    if surf and ticker == 0.25:
-        rect = surf.get_rect(center = bubblerect.center)
-        vista._screen.blit(surf, rect)
+	"""Should be called after the main blit, even after the overlays"""
+	import vista
+	currenttip = ""
+	if not settings.showtips:
+		return
+	if ticker:
+		bubble = graphics.thoughtbubble(bheight, f = ticker*4)
+		bubblerect = bubble.get_rect(midtop = vista.vrect.midtop)
+		vista._screen.blit(bubble, bubblerect)
+	if surf and ticker == 0.25:
+		rect = surf.get_rect(center = bubblerect.center)
+		vista._screen.blit(surf, rect)
 
