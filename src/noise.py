bbc8352979de880877289c730505d4fc7d9caa97
mdtrooper <md@speccy-laptop.(none)>
2013-06-01 02:28:52 +0200

    Improved the source code style.

--- src/noise.py
+++ src/noise.py
@@ -8,53 +8,56 @@ tracks = "ittybitty rocket fighter killing".split()
 sounds = {}
 plays = {}
 def loadsound(name):
-    filename = data.filepath(name + ".ogg")
-    if os.path.exists(filename):
-        sounds[name] = pygame.mixer.Sound(filename)
-        sounds[name].set_volume(settings.soundvolume)
-        plays[name] = []
-    else:
-#        print("sound missing: %s" % name)
-        sounds[name] = None
+	filename = data.filepath(name + ".ogg")
+	if os.path.exists(filename):
+		sounds[name] = pygame.mixer.Sound(filename)
+		sounds[name].set_volume(settings.soundvolume)
+		plays[name] = []
+	else:
+		#print("sound missing: %s" % name)
+		sounds[name] = None
+
 
 def play(name):
-    if settings.silent: return
+	if settings.silent: return
+	
+	if name == "removepart": name = "addpart"
+	if name == "trash": name = "addpart"
+	if name == "addpart":
+		return play("addpart-%s" % random.choice((0,1,2)))
+	if name not in sounds:
+		loadsound(name)
+	if sounds[name] is None:
+		return
+	# Quiet the sound depending on how many times it's played in the last 15 seconds
+	# This is because too many shot noises can get kind of annoying
+	now = time.time()
+	plays[name] = [t for t in plays[name] if t > now - 15]
+	v = max(1 - 0.02 * len(plays[name]), 0.3)
+	sounds[name].set_volume(v * settings.soundvolume)
+	plays[name].append(now)
+	sounds[name].play()
 
-    if name == "removepart": name = "addpart"
-    if name == "trash": name = "addpart"
-    if name == "addpart":
-        return play("addpart-%s" % random.choice((0,1,2)))
-    if name not in sounds:
-        loadsound(name)
-    if sounds[name] is None:
-        return
-    # Quiet the sound depending on how many times it's played in the last 15 seconds
-    # This is because too many shot noises can get kind of annoying
-    now = time.time()
-    plays[name] = [t for t in plays[name] if t > now - 15]
-    v = max(1 - 0.02 * len(plays[name]), 0.3)
-    sounds[name].set_volume(v * settings.soundvolume)
-    plays[name].append(now)
-    sounds[name].play()
 
 def nexttrack():
-    global track
-    if settings.silent: return
-    if track == len(tracks):  # music off
-        pygame.mixer.music.stop()
-        track = 0
-    else:
-        fname = data.filepath(tracks[track] + ".ogg")
-        pygame.mixer.music.load(fname)
-        pygame.mixer.music.play(-1)
-        track += 1
-    vista.musicicontext = font.img(str(track), size = settings.layout.countsize, color=(0,0,0))
+	global track
+	if settings.silent: return
+	if track == len(tracks):  # music off
+		pygame.mixer.music.stop()
+		track = 0
+	else:
+		fname = data.filepath(tracks[track] + ".ogg")
+		pygame.mixer.music.load(fname)
+		pygame.mixer.music.play(-1)
+		track += 1
+	vista.musicicontext = font.img(str(track), size = settings.layout.countsize, color=(0,0,0))
+
 
 def pause():
-    if settings.silent: return
-    pygame.mixer.music.pause()
+	if settings.silent: return
+	pygame.mixer.music.pause()
+
 
 def resume():
-    if settings.silent: return
-    pygame.mixer.music.unpause()
-
+	if settings.silent: return
+	pygame.mixer.music.unpause()
--- src/twinkler.py
+++ src/twinkler.py
@@ -2,55 +2,59 @@ import pygame, random, math
 import vista, mechanics, noise, data, graphics
 
 class Twinkler(object):
-    """A little bit of energy that can be collected by certain organs"""
-    def __init__(self, (x, y)):
-        self.x, self.y = x, y
-        self.vx = self.vy = 0
-        self.t = 0
-        self.sucker = None
-        self.claimed = False
-    
-    def think(self, dt):
-        # TODO: come up with a different motion formula that doesn't depend on framerate
-        if self.sucker:
-            sx, sy = self.sucker.worldpos
-            f = math.exp(-dt)
-            self.vx *= f
-            self.vy *= f
-            self.vx += (sx - self.x) * dt
-            self.vy += (sy - self.y) * dt
-        else:
-            self.ax = random.uniform(-4, 4)
-            self.ay = random.uniform(-4, 4)
-            self.vx += self.ax * dt
-            self.vy += self.ay * dt
-        self.x += self.vx * dt
-        self.y += self.vy * dt
-        self.t += dt
-        if self.sucker:
-            if (self.x - sx) ** 2 + (self.y - sy) ** 2 < 0.3 ** 2:
-                self.claimed = True
-                self.sucker.energize()
-                self.sucker = None
-                noise.play("energize")
-
-    def alive(self):
-        return not self.claimed and (self.sucker or self.t < 5)
-
-    def draw(self):
-        pos = vista.worldtoview((self.x, self.y))
-        alpha = 1 if self.sucker else min(self.t / 0.5, (5 - self.t) / 0.5, 1)
-        img = graphics.twinkler(self.t, alpha)
-        vista.screen.blit(img, img.get_rect(center = pos))
+	"""
+	A little bit of energy that can be collected by certain organs
+	"""
+	def __init__(self, (x, y)):
+		self.x, self.y = x, y
+		self.vx = self.vy = 0
+		self.t = 0
+		self.sucker = None
+		self.claimed = False
+	
+	
+	def think(self, dt):
+		# TODO: come up with a different motion formula that doesn't depend on framerate
+		if self.sucker:
+			sx, sy = self.sucker.worldpos
+			f = math.exp(-dt)
+			self.vx *= f
+			self.vy *= f
+			self.vx += (sx - self.x) * dt
+			self.vy += (sy - self.y) * dt
+		else:
+			self.ax = random.uniform(-4, 4)
+			self.ay = random.uniform(-4, 4)
+			self.vx += self.ax * dt
+			self.vy += self.ay * dt
+		self.x += self.vx * dt
+		self.y += self.vy * dt
+		self.t += dt
+		if self.sucker:
+			if (self.x - sx) ** 2 + (self.y - sy) ** 2 < 0.3 ** 2:
+				self.claimed = True
+				self.sucker.energize()
+				self.sucker = None
+				noise.play("energize")
+	
+	
+	def alive(self):
+		return not self.claimed and (self.sucker or self.t < 5)
+	
+	
+	def draw(self):
+		pos = vista.worldtoview((self.x, self.y))
+		alpha = 1 if self.sucker else min(self.t / 0.5, (5 - self.t) / 0.5, 1)
+		img = graphics.twinkler(self.t, alpha)
+		vista.screen.blit(img, img.get_rect(center = pos))
 
 
 def newtwinklers(mask, dt):
-    ts = []
-    dx, dy = mask.x1 - mask.x0, mask.y1 - mask.y0
-    N = dx * dy * dt * mechanics.twinklerrate
-    for j in range(int(N) + (random.random() < N % 1)):
-        x, y = random.uniform(mask.x0, mask.x1), random.uniform(mask.y0, mask.y1)
-        ts.append(Twinkler((x, y)))
-    return ts    
-
-
+	ts = []
+	dx, dy = mask.x1 - mask.x0, mask.y1 - mask.y0
+	N = dx * dy * dt * mechanics.twinklerrate
+	for j in range(int(N) + (random.random() < N % 1)):
+		x, y = random.uniform(mask.x0, mask.x1), random.uniform(mask.y0, mask.y1)
+		ts.append(Twinkler((x, y)))
+	
+	return ts
